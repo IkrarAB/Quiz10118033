ed0783c77821917ff4127b69bf55569f1b0d9435
Ikrar AB <ikraranugrah26@gmail.com>
2021-05-29 12:47:27 +0700

    Initial commit

---
+++ app/src/main/java/com/ikrarab/quiz10118033/MainActivity.java
@@ -0,0 +1,21 @@
+/*
+* Tanggal Pengerjaan    : Sabtu, 29 Mei 2021
+* NIM                   : 10118033
+* Nama                  : Ikrar Anugrah Bastary
+* Kelas                 : IF-1/S1/VI
+*/
+
+package com.ikrarab.quiz10118033;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
